5982c310e2893ede14a67a8f41cdec838c258ea6
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2018-07-14 17:44:59 -0400

    spec files user_spec.rb not working. The program is not recognizing dependencies. Trying to correct it. (1) added enable sessions to application_controller.rb, (2) added #slug and #.find_by_slug methods to user.rb, (3) added the console (Pry.start) task to the Rakefile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :email, :password
   has_secure_password
+
+  def slug
+    self.username.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|s| s.slug == slug}
+  end
+
 end
