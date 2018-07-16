7cfb0a71de129469686128d9622f0887dd43e9ad
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2018-07-16 16:28:58 -0400

    Added new columns to the users and items table migration files

--- db/migrate/20180714191955_create_items_table.rb
+++ db/migrate/20180714191955_create_items_table.rb
@@ -2,6 +2,8 @@ class CreateItemsTable < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
+      t.string :description
+      t.integer :cost
       t.integer :user_id
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -34,13 +34,18 @@ ActiveRecord::Schema.define(version: 20180714194403) do
 
   create_table "items", force: :cascade do |t|
     t.string  "name"
+    t.string  "description"
+    t.integer "cost"
     t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "username"
+    t.string  "email"
+    t.integer "telephone"
+    t.string  "password_digest"
   end
 
 end
