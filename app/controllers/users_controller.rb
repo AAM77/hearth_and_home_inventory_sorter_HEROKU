808d95b5b909c36af50e7c57a22b799a184bbe02
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2018-07-21 17:02:00 -0400

    (1) added a form to edit_user.erb to allow the user to add and change personal information, (2) added a patch route in users_controller.rb that handles the editing, (3) added a delete route in the users_controller.rb that handles deleting the account (I need to add confirmation somehow), (4) added a corresponding delete form and button to user_info.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   get "/login" do
     if logged_in?
       @user = current_user
-      redirect :"#{@user.slug}"
+      redirect :"/#{@user.slug}"
     else
       erb :"users/login_user"
     end
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "#{@user.slug}"
+      redirect "/#{@user.slug}"
     else
       redirect "/signup"
     end
@@ -50,6 +50,45 @@ class UsersController < ApplicationController
     end
   end
 
+  get "/:slug/edit" do
+    if logged_in?
+      erb :"users/edit_user"
+    else
+      redirect "/login"
+    end
+  end
+
+  patch "/:slug/edit" do
+    if logged_in?
+      @user = current_user
+      @user.first_name = params[:first_name]
+      @user.last_name = params[:last_name]
+      @user.telephone = params[:telephone]
+      @user.email = params[:email]
+
+      #@user.password:
+      if params[:old_password] == @user.password && params[:new_password] == params[:confirm_password]
+        @user.password = params[:new_password]
+      end
+
+      if @user.save
+        redirect "/#{@user.slug}/user_info"
+      else
+        redirect "/#{@user.slug}/edit"
+      end
+
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/:slug/account_details" do
+    if logged_in?
+      @user = current_user
+      erb :"users/user_info"
+    end
+  end
+
   get "/:slug" do
     if logged_in?
       @user = current_user
@@ -58,5 +97,22 @@ class UsersController < ApplicationController
       redirect "/signup"
     end
   end
+
+  delete "/:slug/delete" do
+    if logged_in?
+      @user = current_user
+      if @user.user_id == current_user.id
+        @user.delete
+        redirect "/"
+      else
+        redirect "/#{@user.slug}"
+      end
+
+    else
+      redirect "/login"
+    end
+  end
+
+
 
 end
